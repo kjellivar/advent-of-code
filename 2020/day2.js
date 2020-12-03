6c55f399802bbc78706d756626210c3ce2f22f31
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2020-12-03 11:55:13 +0100

    Day 2

---
+++ 2020/day2.js
@@ -0,0 +1,19 @@
+const lineReader = require('line-reader');
+
+function parseLine(line) { // 2-7 p: pbhhzpmppb
+    const [ positions, allowedChar, password ] = line.split(' ');
+    return {
+        positions: positions.split('-').map(val => parseInt(val, 10) - 1),
+        allowedChar: allowedChar.charAt(0),
+        password
+    };
+}
+
+const validations = [];
+const reader = lineReader.eachLine('./2020/day2.txt', (line, isDone) => {
+    const { positions: [pos1, pos2], allowedChar, password } = parseLine(line);
+    validations.push(password.charAt(pos1) === allowedChar ^ password.charAt(pos2) === allowedChar)
+    if(isDone) {
+        console.log(`Allowed passwords: ${validations.filter(Boolean).length}`);
+    }
+});
