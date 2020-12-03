ca10de684a9d48303f7f805043b4458973d83993
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2020-12-03 12:11:37 +0100

    Day 3

---
+++ 2020/day3.js
@@ -0,0 +1,46 @@
+const lineReader = require('line-reader');
+
+const TREE = '#';
+const SQUARE = '.';
+
+/*
+
+    Right 1, down 1: 90
+    Right 3, down 1: 244
+    Right 5, down 1: 97
+    Right 7, down 1: 92
+    Right 1, down 2 48
+
+*/
+const INCREASERS = [
+    [1,1],
+    [3,1],
+    [5,1],
+    [7,1],
+    [1,2]
+];
+
+const sums = [];
+
+INCREASERS.forEach(([ INCREASER_X, INCREASER_Y ], i) => {
+    let x = 0;
+    let y = 0;
+    let trees = 0;
+    lineReader.eachLine('./2020/day3.txt', (line, isDone) => {
+        if(++y % INCREASER_Y === 0) {
+            x = (x + INCREASER_X) % line.length;
+            if(line.charAt(x) === TREE) {
+                trees++;
+            }
+        }
+
+        if(isDone) {
+            console.log('Amount of trees: ' + trees);
+            sums.push(trees);
+            if(sums.length === INCREASERS.length){
+                console.log(`All multiplied together: ${sums.reduce((prevVal, curVal) => prevVal * curVal, 1)}`)
+            }
+        }
+    });
+});
+
