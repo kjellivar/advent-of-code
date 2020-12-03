c40e3805d9c832d69b0853ca8f886d0afebcca98
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2020-12-03 11:55:03 +0100

    Day 1

---
+++ 2020/day1.js
@@ -0,0 +1,17 @@
+const lineReader = require('line-reader');
+const numbers = [];
+lineReader.eachLine('./2020/day1.txt', (line, isDone) => {
+    numbers.push(parseInt(line, 10));
+
+    if(isDone) {
+        numbers.forEach(num1 => {
+            numbers.forEach(num2 => {
+                numbers.forEach(num3 => {
+                    if((num1 + num2 + num3) === 2020) {
+                        console.log(`num1: ${num1} num2: ${num2} num3: ${num3} - Multiplied: ${num1 * num2 * num3}`);
+                    }
+                })
+            })
+        })
+    }
+});
