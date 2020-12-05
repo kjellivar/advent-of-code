c87239f8f708403c556a64f4a1436ec50beaec2e
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2020-12-05 12:14:25 +0100

    Refactor get-seat-id

--- 2020/05/get-seat-id.js
+++ 2020/05/get-seat-id.js
@@ -3,34 +3,43 @@ const BACK = 'B';
 const LEFT = 'L';
 const RIGHT = 'R';
 
-function getSeatId(boardingNumber) {
-    const nodes = boardingNumber.split('');
+function getSeatId(boardingPass) {
+    const path = boardingPass.split('');
+    const rowPath = path.slice(0, 7);
+    const columnPath = path.slice(7);
 
-    const rowPath = nodes.slice(0, 7);
-    const columnPath = nodes.slice(7);
-
-    const row = traverse([0, 127], rowPath);
-    const column = traverse([0, 7], columnPath);
+    const row = traverse(new BinaryTreeRangeNode(0, 127), rowPath);
+    const column = traverse(new BinaryTreeRangeNode(0, 7), columnPath);
 
     return row * 8 + column;
 }
 
-function traverse([min, max], [direction, ...restPath]) {
-    const mid = (max - min) / 2;
-
+function traverse(node, [direction, ...path]) {
     switch (direction) {
         case FRONT:
         case LEFT:
-            // Keep lower half
-            return restPath.length === 0
-                ? min
-                : traverse([min, Math.floor(min + mid)], restPath);
+            return path.length ? traverse(node.left(), path) : node.min;
         case BACK:
         case RIGHT:
-            // Keep upper half
-            return restPath.length === 0
-                ? max
-                : traverse([Math.ceil(min + mid), max], restPath);
+            return path.length ? traverse(node.right(), path) : node.max;
+    }
+}
+
+class BinaryTreeRangeNode {
+    constructor(min, max) {
+        this.min = min;
+        this.max = max;
+        this.mid = (max - min) / 2;
+    }
+
+    left() {
+        const { min, mid } = this;
+        return new BinaryTreeRangeNode(min, Math.floor(min + mid));
+    }
+
+    right() {
+        const { min, mid, max } = this;
+        return new BinaryTreeRangeNode(Math.ceil(min + mid), max);
     }
 }
 
