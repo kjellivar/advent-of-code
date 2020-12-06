8cbf18e9efa645119d29a25d2fa195a7806dbd34
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2020-12-06 21:49:34 +0100

    Add more range tests

--- test/range.test.js
+++ test/range.test.js
@@ -5,9 +5,12 @@ describe('range', () => {
     it('should return an array of numbers starting at `from` and ending at `to`', () => {
         assert.deepStrictEqual(range(3, 5), [3, 4, 5]);
         assert.deepStrictEqual(range(1, 7), [1, 2, 3, 4, 5, 6, 7]);
+        assert.deepStrictEqual(range(1, 1), [1]);
     });
     it('should handle negative ranges', () => {
         assert.deepStrictEqual(range(-3, 3), [-3, -2, -1, 0, 1, 2, 3]);
         assert.deepStrictEqual(range(3, -3), [3, 2, 1, 0, -1, -2, -3]);
+        assert.deepStrictEqual(range(-3, -3), [-3]);
+        assert.deepStrictEqual(range(-3, -5), [-3, -4, -5]);
     });
 });
