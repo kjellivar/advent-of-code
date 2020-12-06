593940a92bc6134334c4718501123c610b6e0340
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2020-12-06 20:47:42 +0100

    Simplify 2020/05
    Inspired by looking at other solutions and realizing
    seat id was in fact encoded in binary

--- 2020/05/index.js
+++ 2020/05/index.js
@@ -1,5 +1,4 @@
 import { readLines } from '../../lib/read-input.js';
-import { getSeatId } from './get-seat-id.js';
 
 const seatIds = readLines('2020', '05')
     .map(getSeatId)
@@ -17,6 +16,17 @@ function findMissingSeatId() {
             return id;
         }
     }
+}
+
+/**
+ * Thanks to all other AoC'ers who made me understand that boarding pass
+ * is encoded in binary <3
+ * @param {string} boardingPass - example BBFBFBBLRL
+ * @returns {number} seat id
+ */
+function getSeatId(boardingPass) {
+    const binary = boardingPass.replace(/F|L/g, '0').replace(/B|R/g, '1');
+    return parseInt(binary, 2);
 }
 
 export { getSeatId, lastSeatId, findMissingSeatId };
