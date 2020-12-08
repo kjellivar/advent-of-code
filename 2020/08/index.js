429c054401d1ca8637eab25e7e0fb519f776c499
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2020-12-08 10:29:21 +0100

    Reduce size for 2020/08 a bit
    Just to be annoying

--- 2020/08/index.js
+++ 2020/08/index.js
@@ -35,23 +35,12 @@ function part2() {
  */
 function run(program) {
     const hasRun = Array(program.length).fill(false);
-    let pointer = 0;
-    let acc = 0;
+    let [pointer, acc] = [0, 0];
     while (!hasRun[pointer] && pointer !== program.length) {
         const [op, arg] = program[pointer];
         hasRun[pointer] = true;
-        switch (op) {
-            case 'acc':
-                acc += arg;
-                pointer++;
-                break;
-            case 'nop':
-                pointer++;
-                break;
-            case 'jmp':
-                pointer += arg;
-                break;
-        }
+        acc += op == 'acc' ? arg : 0;
+        pointer += op == 'jmp' ? arg : 1;
     }
     return [acc, pointer === program.length];
 }
