daaa632bc392883dea0e543ca15d6f20f042e8ae
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2020-12-09 15:02:56 +0100

    Replace some for-loops with for-of

--- 2020/09/index.js
+++ 2020/09/index.js
@@ -3,18 +3,19 @@ import { readLines } from '../../lib/read-input.js';
 const input = readLines('2020', '09').map((line) => Number(line));
 
 function part1() {
-    for (let i = 25; i < input.length; i++) {
-        const pre = input.slice(i - 25, i); // 0-25,1-26 etc
-        if (!hasSum(pre, input[i])) {
-            return input[i];
+    const entries = input.slice(25).entries();
+    for (const [i, num] of entries) {
+        const pre = input.slice(i, 25 + i); // 0-25,1-26 etc
+        if (!hasSum(pre, num)) {
+            return num;
         }
     }
 }
 
 function part2() {
+    const entries = input.entries();
     const sumToFind = part1();
-    for (let i = 0; i < input.length; i++) {
-        let sum = input[i];
+    for (let [i, sum] of entries) {
         for (let j = i + 1; sum <= sumToFind; sum += input[j++]) {
             if (sum === sumToFind) {
                 const sorted = input.slice(i, j + 1).sort((a, b) => a - b);
@@ -25,11 +26,9 @@ function part2() {
 }
 
 function hasSum(arr, sumToFind) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === sumToFind) {
-                return true;
-            }
+    for (const [i, num1] of arr.entries()) {
+        if (arr.slice(i + 1).some((num2) => num1 + num2 === sumToFind)) {
+            return true;
         }
     }
     return false;
