7780958a5d340ecd32e072eea8415132d02c48b4
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2021-12-06 00:43:45 +0100

    refactor: 2021 day 3

--- src/2021/03.js
+++ src/2021/03.js
@@ -1,56 +1,39 @@
 import assert from 'assert';
 import { readLines } from '../lib/read-input.js';
 
-const input = readLines(2021, 3).map((val) => val.split('').map(Number));
+const input = readLines(2021, 3);
 
-function filterByMostCommonBit(list, pos) {
-    const ones = list.filter((val) => val[pos] === 1);
-    const zeros = list.filter((val) => val[pos] === 0);
+function splitLeastAndMostCommonBits(list, pos) {
+    const ones = list.filter((val) => val.charAt(pos) === '1');
+    const zeros = list.filter((val) => val.charAt(pos) === '0');
     if (ones.length >= zeros.length) {
-        return ones;
+        return [zeros, ones];
     } else {
-        return zeros;
-    }
-}
-
-function filterByLeastCommonBit(list, i) {
-    const ones = list.filter((val) => val[i] === 1);
-    const zeros = list.filter((val) => val[i] === 0);
-    if (ones.length < zeros.length) {
-        return ones;
-    } else {
-        return zeros;
+        return [ones, zeros];
     }
 }
 
 function part1() {
-    let mostCommonBits = '';
-    let leastCommonBits = '';
+    let mostCommonBits = '0b';
+    let leastCommonBits = '0b';
     const length = input[0].length;
     for (let i = 0; i < length; i++) {
-        mostCommonBits += filterByMostCommonBit(input, i)[0][i].toString();
-        leastCommonBits += filterByLeastCommonBit(input, i)[0][i].toString();
+        const [least, most] = splitLeastAndMostCommonBits(input, i);
+        mostCommonBits += most[0].charAt(i);
+        leastCommonBits += least[0].charAt(i);
     }
-    return parseInt(mostCommonBits, 2) * parseInt(leastCommonBits, 2);
+    return Number(mostCommonBits) * Number(leastCommonBits);
 }
 
 function part2() {
-    let i = 0;
-    let co2Scrubber = input;
-    let oxygenGenerator = input;
-    while (co2Scrubber.length > 1) {
-        co2Scrubber = filterByLeastCommonBit(co2Scrubber, i);
-        i++;
+    let [co2, oxygen] = splitLeastAndMostCommonBits(input, 0);
+    for (let i = 1; co2.length > 1 || oxygen.length > 1; i++) {
+        const [least] = splitLeastAndMostCommonBits(co2, i);
+        const [_, most] = splitLeastAndMostCommonBits(oxygen, i);
+        co2 = co2.length > 1 ? least : co2;
+        oxygen = oxygen.length > 1 ? most : oxygen;
     }
-    i = 0;
-    while (oxygenGenerator.length > 1) {
-        oxygenGenerator = filterByMostCommonBit(oxygenGenerator, i);
-        i++;
-    }
-    return (
-        parseInt(co2Scrubber[0].join(''), 2) *
-        parseInt(oxygenGenerator[0].join(''), 2)
-    );
+    return Number(`0b${co2[0]}`) * Number(`0b${oxygen[0]}`);
 }
 
 describe('2021 - Day 3', () => {
