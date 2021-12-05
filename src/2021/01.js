822771590c49437c7f1db6ec24cd22df1f12e782
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2021-12-05 15:09:13 +0100

    2021 day 1

---
+++ src/2021/01.js
@@ -0,0 +1,34 @@
+import assert from 'assert';
+import { readLines } from '../lib/read-input.js';
+
+const depths = readLines(2021, 1).map(Number);
+
+function part1() {
+    const increases = depths
+        .map((val, i) => (i === 0 ? -1 : val - depths[i - 1]))
+        .filter((v) => v > 0).length;
+    return increases;
+}
+
+function part2() {
+    let previousSum;
+    let increases = 0;
+    for (let i = 2; i < depths.length; i++) {
+        const sum = depths[i] + depths[i - 1] + depths[i - 2];
+        if (previousSum && sum > previousSum) {
+            increases++;
+        }
+        previousSum = sum;
+    }
+    return increases;
+}
+
+describe('2021 - Day 1', () => {
+    it('part1 is 1832', () => {
+        assert.strictEqual(part1(), 1832);
+    });
+
+    it('part2 is 1858', () => {
+        assert.strictEqual(part2(), 1858);
+    });
+});
