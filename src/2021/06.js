1d2d3cd06138d8ed27e273449010d83f40950b16
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2021-12-07 04:47:52 +0100

    2021 day 6

---
+++ src/2021/06.js
@@ -0,0 +1,43 @@
+import { URL } from 'url';
+import assert from 'assert';
+import lodash from 'lodash';
+import { readLines } from '../lib/read-input.js';
+
+const FILE_PATH = new URL('', import.meta.url).pathname.split('/');
+const YEAR = parseInt(FILE_PATH[FILE_PATH.length - 2]);
+const DAY = parseInt(FILE_PATH[FILE_PATH.length - 1]);
+
+const input = readLines(YEAR, DAY).flatMap((val) => val.split(',').map(Number));
+
+function part1() {
+    return simulate(80);
+}
+
+function part2() {
+    return simulate(256);
+}
+
+function simulate(days) {
+    const counts = lodash(input.slice()).countBy().value();
+    lodash
+        .range(0, 9)
+        .map(String)
+        .forEach((val) => (counts[val] = counts[val] ?? 0));
+    const fishes = lodash(counts).toArray().value();
+    for (let day = 0; day < days; day++) {
+        const births = fishes.shift();
+        fishes.push(births);
+        fishes[6] += births;
+    }
+    return fishes.reduce((a, b) => a + b);
+}
+
+describe(`${YEAR} - Day ${DAY}`, () => {
+    it('part1 is 380612', () => {
+        assert.strictEqual(part1(), 380612);
+    });
+
+    it('part2 is 1710166656900', () => {
+        assert.strictEqual(part2(), 1710166656900);
+    });
+});
