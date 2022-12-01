fc1e3ea21c0c85659e8d40574e0f28bd8995901b
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2022-12-01 12:26:10 +0100

    WIP 2021 day 9

--- src/2021/09.js
+++ src/2021/09.js
@@ -5,6 +5,16 @@ import { readLines } from '../lib/read-input.js';
 const { sum } = lodash;
 const input = readLines(2021, 9).map((v) => v.split('').map(Number));
 
+/**
+2199943210
+3987894921  l[2][1] < l[i-1][j] && < l[i+1][j] && < l[i][j-1] && < l[i][j+1]
+                    *
+9856789892       *  ↑  *
+8767896789     * <- o -> *
+9899965678       *  ↓  *
+                    * 
+*/
+
 const riskLevel = (height) => 1 + height;
 const getHeight = (x, y) => input?.[x]?.[y] ?? 9;
 
@@ -23,11 +33,19 @@ function getLowPoints() {
     );
 }
 
+function getHighPoints() {
+    return input.map((row) => row.map((height) => (height === 9 ? 'X' : '.')));
+}
+
 function part1() {
     return sum(getLowPoints().map(([x, y]) => riskLevel(input[x][y])));
 }
 
-function part2() {}
+function part2() {
+    // get low points
+    // go left, up, right, down - for each of these points do the same
+    // terminate when height 9
+}
 
 describe(`2021 - Day 9`, () => {
     it('part1 is 436', () => {
