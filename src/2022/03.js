df167faf757a124eef49e5e6f6673ebef7f6af5b
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2022-12-03 08:10:38 +0100

    refactor 2022 day 3

--- src/2022/03.js
+++ src/2022/03.js
@@ -2,25 +2,22 @@ import assert from 'assert';
 import _ from 'lodash';
 import { readLines } from '../lib/read-input.js';
 
-const getPriority = (char) =>
-    char.match(/[a-z]/) ? char.charCodeAt() - 96 : char.charCodeAt() - 38;
+const priorities = (arrs) =>
+    _.uniq(_.intersection(...arrs)).map((char) =>
+        char.match(/[a-z]/) ? char.charCodeAt() - 96 : char.charCodeAt() - 38,
+    );
 
 const input = _(readLines(2022, 3).map((line) => line.split('')));
 
 function part1() {
     return input
-        .flatMap((line) => {
-            const arrs = _.chunk(line, line.length / 2);
-            return _.uniq(_.intersection(...arrs)).map(getPriority);
-        })
+        .map((line) => _.chunk(line, line.length / 2))
+        .flatMap(priorities)
         .sum();
 }
 
 function part2() {
-    return input
-        .chunk(3)
-        .flatMap((group) => _.uniq(_.intersection(...group)).map(getPriority))
-        .sum();
+    return input.chunk(3).flatMap(priorities).sum();
 }
 
 describe(`2022 - Day 3`, () => {
