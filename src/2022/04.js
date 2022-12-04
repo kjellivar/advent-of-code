e13dc2ed057f73ca88e03a5c10022a880d602bc4
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2022-12-04 06:38:33 +0100

    2022 day 4

---
+++ src/2022/04.js
@@ -0,0 +1,23 @@
+import assert from 'assert';
+import _ from 'lodash';
+import { readLines } from '../lib/read-input.js';
+
+const input = readLines()
+    .map((line) => line.split(','))
+    .map((pairs) =>
+        pairs.map((pair) => {
+            const [to, from] = pair.split('-').map(Number);
+            return _.range(to, from + 1);
+        }),
+    );
+
+const part1 = input.filter(
+    ([p1, p2]) => !_.difference(p1, p2).length || !_.difference(p2, p1).length,
+).length;
+
+const part2 = input.filter((pair) => _.intersection(...pair).length).length;
+
+test('2022 - Day 04', () => {
+    assert.strictEqual(part1, 498);
+    assert.strictEqual(part2, 859);
+});
