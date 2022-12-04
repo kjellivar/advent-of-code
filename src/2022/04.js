822bb20078a2cefa8d6a42643d5572df893bc40b
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2022-12-04 09:34:20 +0100

    sort pairs by their assignment size
    prevents us from having to check difference twice,
    as the smaller size list will always be contained in the larger
    (if it is contained in it)
    
    ex:
    [6] - [4, 5, 6, 7] = []

--- src/2022/04.js
+++ src/2022/04.js
@@ -4,12 +4,10 @@ import { readLines } from '../lib/read-input.js';
 
 const input = readLines()
     .map((line) => line.split(',').map((range) => range.split('-').map(Number)))
-    .map((pair) => pair.map(([from, to]) => _.range(from, to + 1)));
+    .map((pair) => pair.map(([from, to]) => _.range(from, to + 1)))
+    .map((pair) => _.sortBy(pair, 'length'));
 
-const part1 = input.filter(
-    ([p1, p2]) => !(_.difference(p1, p2).length && _.difference(p2, p1).length),
-).length;
-
+const part1 = input.filter((pair) => !_.difference(...pair).length).length;
 const part2 = input.filter((pair) => _.intersection(...pair).length).length;
 
 test('2022 - Day 04', () => {
