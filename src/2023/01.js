1ef7ece9ec92736f88ca443cbda9a2a33d349cf1
Kjell Ivar Storstein <kjell.ivar.storstein@finn.no>
2023-12-02 05:27:43 +0100

    2023 day 1

---
+++ src/2023/01.js
@@ -0,0 +1,39 @@
+import assert from 'assert';
+import _ from 'lodash';
+import { readLines } from '../lib/read-input.js';
+
+const lines = _(readLines());
+
+const numMap = new Map([
+    ['one', 'o1e'],
+    ['two', 't2o'],
+    ['three', 't3e'],
+    ['four', 'f4r'],
+    ['five', 'f5e'],
+    ['six', 's6x'],
+    ['seven', 's7n'],
+    ['eight', 'e8t'],
+    ['nine', 'n9e'],
+]);
+
+function part1(input = lines) {
+    return input
+        .map((line) => line.split('').filter(Number))
+        .map((numbers) => Number(numbers.at(0) + numbers.at(-1)))
+        .sum();
+}
+
+function part2() {
+    const input = lines.map((line) => {
+        for (const [key, val] of numMap) {
+            line = line.replaceAll(key, val);
+        }
+        return line;
+    });
+    return part1(input);
+}
+
+test('2023 - Day 01', () => {
+    assert.strictEqual(part1(), 54708);
+    assert.strictEqual(part2(), 54087);
+});
